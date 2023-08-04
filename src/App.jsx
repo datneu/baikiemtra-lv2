416a1ae0394a978cfe86cdbc08248f83509a6cd6
vubao2001 <levubao2001@gmail.com>
2023-08-04 21:23:43 +0700

    BKT cuoi khoa

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import TodoListPage from './components/TodoListPage'
+
+function App() {
+
+  return (
+    <>
+      <TodoListPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import styles from './TodoListPage.module.css'
+import { MdDeleteOutline } from "react-icons/md";
+
+const TodoItem = (props) => {
+    const { menuActive, todo, reload, setReload, deleteTodo } = props;
+    const [teste, setTeste] = useState(todo.checked);
+
+    function handleCheckbox(event) {
+        setTeste(event.target.checked);
+        todo.checked = event.target.checked;
+
+        setReload(!reload);
+    }
+
+    useEffect(() => { }, [teste, todo.checked]);
+
+    return (
+        <>
+            <div className={styles.todoContainer}>
+                <div className={styles.todo}>
+                    <input
+                        type="checkbox"
+                        name="checkbox"
+                        id={todo.id}
+                        className={styles.checkbox}
+                        onChange={handleCheckbox}
+                        checked={todo.checked}
+                    />
+
+                    <label htmlFor={todo.id} className={styles.label}>
+                        {todo.text}
+                    </label>
+                </div>
+
+                {
+                    menuActive === "completed" ? (<MdDeleteOutline className={styles.deleteIcon} onClick={() => deleteTodo(todo.id)} />) : null
+                }
+            </div>
+        </>
+    )
+}
+
+export default TodoItem
